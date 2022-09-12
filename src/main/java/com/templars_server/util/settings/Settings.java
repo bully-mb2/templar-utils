839639ae0471dd8933d6373898d839721088dcce
Bully <bully.mb2@gmail.com>
2022-09-12 18:03:19 +0200

    Make Settings write missing default values upon loading

--- src/main/java/com/templars_server/util/settings/Settings.java
+++ src/main/java/com/templars_server/util/settings/Settings.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.util.Map;
 
 public class Settings {
 
@@ -18,26 +19,64 @@ public class Settings {
 
     /**
      *
-     * Loads a config file relative to the executable.
-     * If no config can be found it will try to create one
+     * Loads a properties file relative to the executable.
+     * If no properties file can be found it will try to create one
      * from a template that should be located in the classpath resources.
      *
      * @param pathname path to settings file
-     * @throws IOException if the template file could not be located
+     * @throws IOException if the template file could not be read
      */
     public void load(String pathname) throws IOException {
-        properties = new Properties();
         LOG.info("Loading config");
+        properties = new Properties();
         File file = new File(pathname);
         if (file.exists()) {
             try (FileInputStream stream = new FileInputStream(file)) {
                 properties.load(stream);
+                LOG.info("Loading any missing properties");
+                loadDefaults(pathname, false);
             }
         } else {
-            try (FileOutputStream stream = new FileOutputStream(file)) {
-                LOG.info("No config found, creating from default");
-                properties.load(Settings.class.getResourceAsStream("/" + pathname));
-                properties.store(stream, null);
+            LOG.info("No config found, creating from default");
+            loadDefaults(pathname, true);
+        }
+
+        store(pathname);
+    }
+
+    /**
+     *
+     * Stores the properties to a file
+     *
+     * @param pathname path to properties file
+     * @throws IOException if it can't create the file
+     */
+    public void store(String pathname) throws IOException {
+        LOG.info("Storing config");
+        try (FileOutputStream stream = new FileOutputStream(pathname)) {
+            properties.store(stream, null);
+        }
+    }
+
+    /**
+     *
+     * Loads the default properties file template from classpath resources
+     *
+     * @param pathname path to settings file
+     * @param overwrite if the defaults should overwrite existing keys or not
+     * @throws IOException if the template file could not be read
+     */
+    public void loadDefaults(String pathname, boolean overwrite) throws IOException {
+        LOG.info("Loading default config");
+        Properties defaults = new Properties();
+        try (InputStream stream = Settings.class.getResourceAsStream("/" + pathname)) {
+            defaults.load(stream);
+            for (Map.Entry<Object, Object> entry : defaults.entrySet()) {
+                if (overwrite) {
+                    properties.put(entry.getKey(), entry.getValue());
+                } else {
+                    properties.putIfAbsent(entry.getKey(), entry.getValue());
+                }
             }
         }
     }
